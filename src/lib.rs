405dec440ce332016ca0bce92190ffbf7585864d
vinsburg <vinsburg@gmail.com>
2024-05-04 14:11:42 +0300

    Now passes the sequence by reference

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,14 @@ pub struct Stats {
 }
 
 #[allow(dead_code)]
-pub fn calc_stats(numbers: Vec<i32>) -> Stats {
-    let mut cur_min: i32 = numbers[0];
-    let mut cur_max: i32 = numbers[0];
+pub fn calc_stats(numbers: &Vec<i32>) -> Stats {
+    // copy the first element to initialize the min and max:
+    let nums = (*numbers).clone();
+    let mut cur_min: i32 = nums[0];
+    let mut cur_max: i32 = nums[0];
     let mut length: i32 = 0;
     let mut cur_sum: i32 = 0;
-    for num in numbers {
+    for num in nums {
         length += 1;
         cur_sum += num;
         if num < cur_min {
@@ -36,28 +38,28 @@ mod tests {
     #[test]
     fn test_min_is_1() {
         let numbers = vec![1, 2, 3, 4, 5];
-        let stats = calc_stats(numbers);
+        let stats = calc_stats(&numbers);
         assert_eq!(stats.min, 1);
     }
 
     #[test]
     fn test_max_is_5() {
         let numbers = vec![1, 2, 3, 4, 5];
-        let stats = calc_stats(numbers);
+        let stats = calc_stats(&numbers);
         assert_eq!(stats.max, 5);
     }
 
     #[test]
     fn test_len_is_5() {
         let numbers = vec![1, 2, 3, 4, 5];
-        let stats = calc_stats(numbers);
+        let stats = calc_stats(&numbers);
         assert_eq!(stats.len, 5);
     }
 
     #[test]
     fn test_mean_is_3() {
         let numbers = vec![1, 2, 3, 4, 5];
-        let stats = calc_stats(numbers);
+        let stats = calc_stats(&numbers);
         assert_eq!(stats.mean, 3.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use calc_stats::calc_stats;
 
 fn main() {
     let numbers = vec![5, 2, 9, 1, 5, 6, 7, 8, 2, 3, 0, 4, 5, 6, 7, 8, 9];
+    let stats = calc_stats(&numbers);
     println!("The input sequence is:\n\t{:?}", numbers);
-    let stats = calc_stats(numbers);
     println!(
         "The statistic properties are:\n\tMin: {}, Max: {}, Length: {}, Mean: {}",
         stats.min, stats.max, stats.len, stats.mean
